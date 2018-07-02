43875d86b0ff9d2377b6a629a03b8db8c94d31a9
WeiKai Liao <34786918+karevin07@users.noreply.github.com>
2018-07-02 17:18:24 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 set -e
 
-#查看mysql服务的状态，方便调试，这条语句可以删除
+#查看mysql服務的狀態，方便測試，這條命令可以删除
 echo `service mysql status`
 
 echo '1.啟動mysql....'
-#启动mysql
+#啟動mysql
 service mysql start
 sleep 3
 echo `service mysql status`
 
 echo '2.載入資料....'
-#导入数据
+#載入資料
 mysql < /mysql/script.sql
 echo '3.載入資料完畢....'
 
 sleep 3
 echo `service mysql status`
 
-#重新设置mysql密码
+#重新設置mysql密碼
 echo '4.開始修改密碼....'
 mysql < /mysql/privileges.sql
 echo '5.修改密碼完畢....'
