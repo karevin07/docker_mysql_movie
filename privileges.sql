94a1810969790602737e01fa8bd53660b7ff1e74
WeiKai Liao <34786918+karevin07@users.noreply.github.com>
2018-07-02 17:17:01 +0800

    Update privileges.sql

--- privileges.sql
+++ privileges.sql
@@ -1,8 +1,8 @@
 use mysql;
 select host, user from user;
--- 因为mysql版本是5.7，因此新建用户为如下命令：
+-- 因為mysql版本是5.7，因此新建用户為如下命令：
 create user docker identified by '123456';
--- 将docker_mysql数据库的权限授权给创建的docker用户，密码为123456：
+-- 将docker_mysql資料庫的權限授權给創建的docker用户，密碼為123456：
 grant all on docker_mysql.* to docker@'%' identified by '123456' with grant option;
--- 这一条命令一定要有：
+-- 這一條命令一定要有：
 flush privileges;
